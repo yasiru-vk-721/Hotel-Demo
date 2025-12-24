170874e4546352c08b062224a7d063dbd02a29ff
yasiru-vk-721 <yasiru.k721@gmail.com>
2025-12-24 19:03:00 +0530

    Initial commit all

---
+++ src/app/rooms/page.tsx
@@ -0,0 +1,8 @@
+// app/rooms/page.tsx
+"use client";
+
+import Rooms from "@/components/Rooms";
+
+export default function RoomsPage() {
+  return <Rooms />;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-outfit)", "sans-serif"],
+      },
+    },
+  },
+};
